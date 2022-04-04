4a8af92bc920b3d3027b6778afe1ec7e909176f6
Pawel Maczuga <pmaczuga97@gmail.com>
2022-04-04 10:56:03 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/TerrainGraphs.jl
@@ -0,0 +1,5 @@
+module TerrainGraphs
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using TerrainGraphs
+using Test
+
+@testset "TerrainGraphs.jl" begin
+    # Write your tests here.
+end
